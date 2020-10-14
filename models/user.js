5cdd5493ab6ff8f5ccf902d198f0ccf07311a497
Romulo S. Ramos <rsromulo@msn.com>
2020-10-14 18:10:26 -0300

    hashing passwords

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email:{
@@ -22,8 +23,10 @@ userSchema.post('save',function(doc,next){
 })
 
 // fire a function before doc saved to db
-userSchema.pre('save',function(next){
-    console.log('user about to be created & saved',this);
+userSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password,salt);
+    //console.log('user about to be created & saved',this);
     next();
 })
 
