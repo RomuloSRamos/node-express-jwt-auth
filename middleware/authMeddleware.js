f4b8ddeff5e9a94bb92d25ce8d7f03a433c2f721
Romulo S. Ramos <rsromulo@msn.com>
2020-10-16 12:16:19 -0300

    Checking the Current User

--- middleware/authMeddleware.js
+++ middleware/authMeddleware.js
@@ -1,6 +1,7 @@
 const { JsonWebTokenError } = require("jsonwebtoken");
 
 const jwt = require('jsonwebtoken');
+const { User } = require('../models/user');
 
 const requireAuth = (req, res, next) => {
     const token = req.cookies.jwt;
@@ -21,4 +22,29 @@ const requireAuth = (req, res, next) => {
         res.redirect('/login');
     }
 }
-module.exports = { requireAuth };
+
+// check current user
+const checkUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+    // check json web token exists & verified
+    if(token){
+        jwt.verify(token,'net ninja secret', async (err,decodedToken) =>{
+            if(err){
+                console.log(err.message);
+                res.locals.user = null;
+                next();
+            } else {
+                console.log(decodedToken);
+                let user = await User.findById(decodedToken.id);
+                res.locals.user = user;
+                next();
+            }
+        })
+    }
+    else{
+        res.locals.user = null;
+        next();
+    }
+}
+
+module.exports = { requireAuth, checkUser};
